175072612b7caca0137c4ea194d807597209440c
Luis-Quispe-dev <u20231b869@upc.edu.pe>
2026-05-12 14:24:09 -0500

    Initial commit

---
+++ src/main/java/com/upc/ss/MemoriaVivaTechApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaVivaTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriaVivaTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/ss/MemoriaVivaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaVivaTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
