b4ede31a0d73c29a58af860c1fc4869a14c13da0
Luis-Quispe-dev <u20231b869@upc.edu.pe>
2026-06-11 14:10:58 -0500

    Agregando contenido necesario para la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (name) VALUES ('ROLE_ADULTO_MAYOR');
+INSERT INTO roles (name) VALUES ('ROLE_CUIDADOR');
+INSERT INTO plan (nombre_plan, precio, limite_recuerdos, descripcion) VALUES ('Libreta Familiar', 0.00, 100, 'Gratis sin publicidad');
+INSERT INTO plan (nombre_plan, precio, limite_recuerdos, descripcion) VALUES ('Album Familiar', 9.99, 300, 'Cancelas cuando quieras - Gratis sin publicidad');
+INSERT INTO plan (nombre_plan, precio, limite_recuerdos, descripcion) VALUES ('Biblioteca Familiar', 19.99, 600, 'Más calidad de descarga - Cancelas cuando quieras - Gratis sin publicidad');
