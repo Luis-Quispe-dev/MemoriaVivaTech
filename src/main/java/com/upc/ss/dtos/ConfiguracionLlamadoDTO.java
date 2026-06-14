14b28238619aa6fcfbc055dadf80c8916b20de6b
LuisArenas2006 <u202319381@upc.edu.pe>
2026-06-14 18:16:06 -0500

    Insertacion del codigo del dto ConfiguracionLlamadoDTO

--- src/main/java/com/upc/ss/dtos/ConfiguracionLlamadoDTO.java
+++ src/main/java/com/upc/ss/dtos/ConfiguracionLlamadoDTO.java
@@ -1,1 +1,30 @@
+package com.upc.ss.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ConfiguracionLlamadoDTO {
+    @NotNull(message = "El id del usuario es obligatorio")
+    private Long idUsuario;
+
+    @NotBlank(message = "El idioma es obligatorio")
+    private String idioma;
+
+    @NotBlank(message = "El tamaño de fuente es obligatorio")
+    private String tamanioFuente;
+
+    @NotBlank(message = "El tema visual es obligatorio")
+    private String temaVisual;
+
+    @NotNull(message = "Las notificaciones son obligatorias")
+    private Boolean notificacionesSonido;
+}
