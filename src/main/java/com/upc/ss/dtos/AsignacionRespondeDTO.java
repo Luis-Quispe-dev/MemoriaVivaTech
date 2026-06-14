e315c8262bb85a80a26228c541010e1e18878002
LuisArenas2006 <u202319381@upc.edu.pe>
2026-06-14 18:19:44 -0500

    Reestructuracion del codigo del dto AsignacionRespondeDTO

--- src/main/java/com/upc/ss/dtos/AsignacionRespondeDTO.java
+++ src/main/java/com/upc/ss/dtos/AsignacionRespondeDTO.java
@@ -1,1 +1,27 @@
+package com.upc.ss.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AsignacionRespondeDTO {
+    private Long idAsignacion;
+    private Long idAdultoMayor;
+    private String nombreAdultoMayor;
+    private Long idCuidador;
+    private String nombreCuidador;
+    private Long idSolicitud;
+    private String estado;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+    private String fotoAdultoMayor;
+    private String fotoCuidador;
+}
